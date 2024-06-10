fcd3334181b1da78cf76925d9478605733f0dbeb
lin_wsl <hao.lin0728@gmail.com>
2024-06-10 11:56:10 -0400

    changed react app to use index.jsx and deleted main.jsx

--- snack-or-booze/src/index.jsx
+++ snack-or-booze/src/index.jsx
@@ -1,13 +1,17 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import ReactDOM from "react-dom";
-import {createRoot, hydrateRoot} from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+  
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
