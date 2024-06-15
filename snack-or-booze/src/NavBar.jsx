dba56aeb05c904642e5f76b32a6cb8bdf766cb48
lin_wsl <hao.lin0728@gmail.com>
2024-06-15 14:31:40 -0400

    NavBar v1 added

---
+++ snack-or-booze/src/NavBar.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
+import PropTypes from "prop-types";
+import "./NavBar.css";
+
+const NavBar = ({ direction = "down", ...args }) => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggle = () => setDropdownOpen((prevState) => !prevState);
+
+  return (
+    <div className="d-flex p-5">
+      <Dropdown isOpen={dropdownOpen} toggle={toggle} direction={direction}>
+        <DropdownToggle caret>Snack or Booze</DropdownToggle>
+        <DropdownMenu {...args}>
+          <DropdownItem header>Menu</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>
+            <Link to="/">Home</Link>
+          </DropdownItem>
+          <DropdownItem>
+            <Link to="/snacks">Snacks Menu</Link>
+          </DropdownItem>
+          <DropdownItem>
+            <Link to="/drinks">Snacks Menu</Link>
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    </div>
+  );
+};
+
+export default NavBar;
