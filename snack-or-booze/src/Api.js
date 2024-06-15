ad406901e3a125b55028049867422075d1e63459
lin_wsl <hao.lin0728@gmail.com>
2024-06-14 22:34:58 -0400

    v1 new class methods added to SnackOrBoozeApi

--- snack-or-booze/src/Api.js
+++ snack-or-booze/src/Api.js
@@ -10,12 +10,44 @@ const BASE_API_URL = "http://localhost:5000";
 */
 
 class SnackOrBoozeApi {
-
+  //Snacks static methods
   static async getSnacks() {
-    const result = await axios.get(`${BASE_API_URL}/snacks`);
+    try {
+      const result = await axios.get(`${BASE_API_URL}/snacks`);
+      console.log("Snacks retrieved successfully:", response.data);
+    } catch (error) {
+      console.error("Error retrieving snack data:", error);
+    } finally {
+      return response.data;
+    }
+  }
+  static async postSnack(snackData) {
+    try {
+      const response = await axios.post("/snack", snackData);
+      console.log("Snack posted successfully:", response.data);
+    } catch (error) {
+      console.error("Error posting snack:", error);
+    } finally {
+      return response.data;
+    }
+  }
+
+  //Drinks static methods
+  static async getDrinks() {
+    const result = await axios.get(`${BASE_API_URL}/drinks`);
     return result.data;
   }
 
+  static async postDrink(drinkData) {
+    try {
+      const response = await axios.post("/Drink", drinkData);
+      console.log("Drink posted successfully:", response.data);
+    } catch (error) {
+      console.error("Error posting Drink:", error);
+    } finally {
+      return response.data;
+    }
+  }
 }
 
 export default SnackOrBoozeApi;
